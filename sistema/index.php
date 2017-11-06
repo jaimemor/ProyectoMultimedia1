84ee192ca5beb4b7f77932748f19ab5c74c03cd7
marceloibarra <marceloibarra06@gmial.com>
2017-11-06 11:16:27 -0300

    informacion

--- sistema/index.php
+++ sistema/index.php
@@ -1,1 +1,8 @@
-indexjaj
+secretaria
+->mostrar
+listado de salas con su informacion correspondiente
+
+(capacidad, implementos,profesores que la ocupan, agregar comentarios y mostrar)
+desplegar horarios de salas en otra pestaña
+
+
