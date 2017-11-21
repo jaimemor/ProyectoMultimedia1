65aafbf767a81c5c2ad45e10c32e7f2fa88c2e9d
marceloibarra <marceloibarra06@gmial.com>
2017-11-21 20:23:55 -0300

    index editado

--- inicio.php
+++ inicio.php
@@ -13,144 +13,91 @@
 
 
 
-  
-<div class="col-md-12 " style="background: #4682B4 ;">
+
+<div class="col-md-12 " style="background: #87CEEB ;">
 
   
   <div class="left">SISTEMA SALAS</div>
 
   <div class="right"><button type="button" class="btn btn-primary">Login</button></div>
-
-
-
-
 </div>
-
-<div class="container-fluid" class="left">
-  <div class="row" class="left">
-
-
-
-
-  </div></div>
-
 
 
 
 
 <div class="row">
-  <div class="col-md-9 col-md-push-3"><div class="left"><button type="button" class="btn btn-primary">otro boton</button></div>
-  <div class="left"><button type="button" class="btn btn-primary">ejemplo de boton</button></div>
 
- </div>
+
+  <!--
+        botonera
+
+    -->
+    <div class="col-xs-6 col-md-4 col-lg-2 vcenter">
+        <div style="height:30em;border:10px solid #FFF">
+          
+<div><h4>ADMINISTRACION</h4></div>
+<div class="btn-group-vertical " style="border:0px solid #fff" >
+
+
+
+<div class="btn-group" style="border:0px solid #fff">
+
+  <button type="button" class="btn btn-default"><h3>inicio</h3></button>
+  <button type="button" class="btn btn-default">otro boton</button>
+  <button type="button" class="btn btn-default">solicitar sala</button>
+ 
+ 
   
 
-  <div class="col-md-3 col-md-pull-9">
-    
 
-<div>Usuario: <?php // echo $_SESSION['login']; ?></div>
-<div>
-  
-  Año:<select name="ano">
-        <?php
-        for($i=date('o'); $i>=2010; $i--){
-            if ($i == date('o'))
-                echo '<option value="'.$i.'" selected>'.$i.'</option>';
-            else
-                echo '<option value="'.$i.'">'.$i.'</option>';
-        }
-        ?>
-</select>
+<div class="btn-group">
+  <button type="button" class="btn btn-default dropdown-toggle"
+          data-toggle="dropdown">
+    Título del botón <span class="caret"></span>
+  </button>
+ 
+  <ul class="dropdown-menu" role="menu">
+    <li><a href="#">Acción #1</a></li>
+    <li><a href="#">Acción #2</a></li>
+    <li><a href="#">Acción #3</a></li>
+    <li class="divider"></li>
+    <li><a href="#">Acción #4</a></li>
+  </ul>
 </div>
 
-<div>
-  Semestre:<br>
-  <input type="radio" name="semestre" value="primersemenstre" required="">Primer Semestre <br>
-  <input type="radio" name="semestre" value="segundosemestre" required="">Segundo Semestre<br>
-    
-</div>
-  
-  <div>
-    
 
-    Seleccione piso:  <select name="piso" id="">
-        <option name="" value="piso">piso 1</option>
-        <option value="piso">piso 2</option>
-      </select>
-      
+
+
+
+</div>
+
+
+
 
   </div>
 
-  </div>
-</div>
 
+        </div>
+    </div>
 
+<div class="col-xs-6 col-md-8 col-lg-10 vcenter" >
+        <div style="height:10em;border:10px solid #fff">
+          <h1>salas</h1>
+        </div>
+        </div>
+<!--
+        aqui se muestran las salas
 
+    -->
 
-<div class="container-fluid">
-    
-         <div class="row">
-
-       
-
-
-
-<div class="col-md-12 " class="center" style="background: #fff ;">
+<div class="col-xs-6 col-md-8 col-lg-10 vcenter" style="background: #F8F8FF ;">
+        <div style="height:20em;border:10px solid #fff">
+   
 
  	
-<div>Usuario: <?php // echo $_SESSION['login']; ?></div>
-<div>
-	
-	Año:<select name="ano">
-        <?php
-        for($i=date('o'); $i>=2010; $i--){
-            if ($i == date('o'))
-                echo '<option value="'.$i.'" selected>'.$i.'</option>';
-            else
-                echo '<option value="'.$i.'">'.$i.'</option>';
-        }
-        ?>
-</select>
-</div>
-
-<div>
-	Semestre:<br>
-	<input type="radio" name="semestre" value="primersemenstre" required="">Primer Semestre <br>
-	<input type="radio" name="semestre" value="segundosemestre" required="">Segundo Semestre<br>
-		
-</div>
-	
-	<div>
-		
-
-    Seleccione piso:  <select name="piso" id="">
-      	<option name="" value="piso">piso 1</option>
-      	<option value="piso">piso 2</option>
-      </select>
-      
-
-  </div>
-  </div>
-  </div>
-	</div>
-
-		 <h1>salas</h1>
-
-<div class="container-fluid">
-  
-         <div class="row">
-
-          <div class="col-md-1 " style="background: #fff ;"></div>
-<div class="col-md-10 " style="background: #fff ;">
-
-
-
-
 
 <?php
   
-
-
     include "conec.php";
 
   $sql="select * from sala ";
@@ -159,17 +106,9 @@
   $resultado=$smt->fetchall ();
   $conn =null;
   $var= count ($resultado);
- 
-  
-  
-  ?>                                                   
+   ?>                                                   
      
- 
-
-  
-  
-
-<table  border-color="#ffffff" border="1" class="table table-hover">
+<table style="border:10px solid #ccc"  class="table table-hover">
  
  <?php
 
@@ -184,10 +123,10 @@
  
 
     for ($i=0; $i < $var; $i++) { 
-      echo "<table 
-       border-color='#cccccc' border='1' >
-       <tr><th>sala<td>".$resultado[$i]['codsala']."</td></th></tr>
-       <tr><th>implemento<td>".$resultado[$i]['implemento']."</td></th></tr>
+      echo "<table style='border:1px solid #ccc'  class='table table-hover'>
+
+       <tr><th>".$resultado[$i]['codsala']."</th></tr>
+       
 
 
 
@@ -203,10 +142,13 @@
   
 
 
-          <div class="col-md-1 " style="background: #fff ;"></div>
+          
 
 
-     
+     </div>
+    </div>
+
+</div>
 
  </body>
 
