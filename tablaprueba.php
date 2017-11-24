437049954ea8539ef15d3804c7ee99b48d418892
marceloibarra <marceloibarra06@gmial.com>
2017-11-24 12:56:39 -0300

    colores finales

--- tablaprueba.php
+++ tablaprueba.php
@@ -3,7 +3,20 @@
 
 
 
-<div class="col-md-4 " style="background:     #bedddb      ;"> 
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title></title>
+ <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body>
+  <div class="col-md-4 " style="background:     #bedddb      ;"> 
 
 asdasdasda
 </div>
@@ -17,6 +30,7 @@ asdasdsa
 
 
 <button type="button" class="btn btn-primary">Login</button>
-
-
-
+</body>
+<script src="js/bootstrap.min.js" type="text/javascript" charset="utf-8" async defer></script>
+    <script src="js/jquery-3.2.1.min.js" type="text/javascript" charset="utf-8" async defer></script>
+</html>
